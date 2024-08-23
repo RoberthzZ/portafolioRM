544dc8d5100cd7b47f4bf707c70e68df5a5555e2
Roberto Maya Narvaez <administrativo3@gmail.com>
2024-08-23 13:22:48 -0400

    subida de archivos a repositorio

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function mostrarGale(){
+    document.getElementById('galeria').style.display = 'block';
+}
+function ocultarGale(){
+    document.getElementById('galeria').style.display = 'none';
+}
+
+function mostrarMensaje(){
+    document.getElementById('caja2').style.display = 'block';
+}
+
+function ocultarMensaje(){
+    document.getElementById('caja2').style.display = 'none';
+}
